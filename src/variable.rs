7657973f785c8364dc94a259498a92d571bc9dbd
Emanuele-01 <emanuele.pitoni@merendels.com>
2025-08-14 12:10:46 +0200

    Add immutable variable example and documentation

--- src/variable.rs
+++ src/variable.rs
@@ -1,9 +1,20 @@
 pub fn variable_print() {
-
+    /*
+    This function prints the value of a mutable variable.
+     It demonstrates the concept of mutable variables in Rust.
+    Mutable variables can be changed after they are initialized.
+    */
     let mut x = 5;
     println!("The value of x is: {}", x);
     x = 6;
     println!("The value of x is: {}", x);
 
-
+    /*
+     This function prints the value of an immutable variable.
+    It demonstrates the concept of immutable variables in Rust.
+    Immutable variables cannot be changed after they are initialized.
+    not be changed after they are initialized.
+    */
+    let y = 10;
+    println!("The value of y is: {}", y);
 }
