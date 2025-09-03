9bccc70dbc8af2b51661727153fa44722af3a2cd
Emanuele-01 <emanuele.pitoni@merendels.com>
2025-09-03 17:19:20 +0200

    Adds documentation for common Cargo commands
    Introduces a detailed comment explaining four commonly used
    Cargo commands: `cargo new`, `cargo build`, `cargo run`, and
    `cargo check`. This improves code clarity and provides helpful
    insights for developers working with Rust projects.

--- src/main.rs
+++ src/main.rs
@@ -16,3 +16,13 @@ fn main() {
  */
 
 //  this is a single-line comment
+
+/*
+We have four commands for using Cargo during our development workflow with Rust.
+
+- Cargo new: to create a new project in Rust.
+- Cargo build: to compile our project. Usually, a target folder is created and the compiled code is placed inside it.
+- Cargo run: to run the project. It mainly executes a Cargo Build first and then executes the compiled code, all in a single command.
+- Cargo Check: to verify if the project can be compiled without errors.
+
+*/
