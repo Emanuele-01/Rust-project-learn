920b443311fdd804af2ef9a6656c4cdf86087d19
Emanuele Pitoni <emanuele.pitoni.2001@gmail.com>
2025-09-06 20:03:06 +0200

    create new version: 1.0.1

--- src/scripts/newVersion.sh
+++ src/scripts/newVersion.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
-version="1.0.0"
+version="1.0.1"
 
-git tag -a $version -m "create new version: $version"
-
-git push origin --tags
-
+# Stage and commit changes first
 git add .
-
 git commit -m "create new version: $version"
 
+# Create and push the tag
+git tag -a $version -m "create new version: $version"
 git push origin $version
 
