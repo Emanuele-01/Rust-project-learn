23982b4ff82d596d924f118f0f1e8bd75f6b0a5d
Emanuele Pitoni <emanuele.pitoni.2001@gmail.com>
2025-09-13 18:22:35 +0200

    create new version: v1.0.6

--- src/scripts/newVersion.sh
+++ src/scripts/newVersion.sh
@@ -4,13 +4,13 @@ lastVersion="$1"
 version="$2"
 
 sed -i '' "s|$lastVersion|$version|g" ./Cargo.toml
-sed -i '' "s|$lastVersion|$version|g" ./VERSION
+sed -i '' "s|v$lastVersion|v$version|g" ./VERSION
 
 # Stage and commit changes first
 git add .
-git commit -m "create new version: $version"
+git commit -m "create new version: v$version"
 
 # Create and push the tag
-git tag -a $version -m "create new version: $version"
-git push origin $version
+git tag -a v$version -m "create new version: v$version"
+git push origin v$version
 
